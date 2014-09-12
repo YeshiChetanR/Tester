7f5f55594cb86c241f8c73f489bdc9bee7d99336
Chetan <chetan@ATMECSINDT-049.(none)>
2014-09-12 11:37:01 +0530

    initial commit comment

---
+++ src/main/java/com/atmecs/tester/App.java
@@ -0,0 +1,40 @@
+package com.atmecs.tester;
+
+import org.hamcrest.Factory;
+import org.json.simple.parser.ParseException;
+import org.testng.annotations.Test;
+
+import com.atmecs.restful.executor.FactoryTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws ParseException
+    {
+    	/*Sample s = new Sample();
+    	s.runTestNGTest();*/
+    }
+    
+    
+	
+	/*@Test
+	 public void main( String[] args ) 
+	    {
+		System.out.println("in test");
+	    	Sample s = new Sample();
+	    	s.runTestNGTest();
+	    }*/
+   
+	/*@Factory
+	public Object[] createTestCaseInstance() {
+
+			Object[] controller = new Object[1];
+		    controller[0] = new FactoryTest("Config.properties", "suite.json");
+			return controller;
+	
+	}*/
+	
+	}
